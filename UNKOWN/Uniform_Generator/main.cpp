5e384550d666cb7fd0dd7454e1bcb5be56234d34
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-09-21 22:59:47 +0200

    Uniform Generator

---
+++ UNKOWN/Uniform_Generator/main.cpp
@@ -0,0 +1,38 @@
+#include <iomanip>
+#include <ios>
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+  while (b != 0) {
+    int t = b;
+    b = a % b;
+    a = t;
+  }
+  return a;
+}
+
+void solve(long long s, long long m) {
+  cout << setw(10) << s << " " << setw(9) << m;
+  if (gcd(s, m) == 1) {
+    cout << setw(15) << right << "Good Choice";
+  } else {
+    cout << setw(14) << right << "Bad Choice";
+  }
+  cout << endl << endl;
+}
+
+int main() {
+  long long s, m;
+  // for (int i = 1; i <= 30; i++) {
+  //   if (i == 10 || i == 20 || i == 25) {
+  //     cout << "x";
+  //   } else {
+		// 	cout << ".";
+		// }
+  // }
+  // cout << endl;
+  while (cin >> s >> m) {
+    solve(s, m);
+  }
+}
