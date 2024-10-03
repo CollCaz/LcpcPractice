f9ff41678b864a64123bbba3537523da657c4bf8
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-10-03 14:44:24 +0200

    Sort the array

--- 1000-1500/1300-Sort_the_Array/main.cpp
+++ 1000-1500/1300-Sort_the_Array/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -6,59 +7,48 @@ int main() {
   int n;
   cin >> n;
   vector<int> line;
-  line.push_back(0);
   for (int i = 0; i < n; i++) {
     int e;
     cin >> e;
     line.push_back(e);
   }
-  bool r = false;
-  bool rEnded = false;
-  int s = 0;
-  int e = 0;
-  bool good = false;
-  bool preSorted = false;
-  // 3 1 2 4$
-  //   s e
-  for (int i = 1; i <= n; i++) {
-    // cout << line[i] << " < " << line[i - 1] << endl;
-    if (rEnded && line[i] < line[i - 1]) {
-      good = false;
+  int l = -1, r = -1;
+  for (int i = 0; i < n - 1; i++) {
+    if (l == -1 && line[i] > line[i + 1]) {
+      l = i;
     }
-    if (!r && line[i] < line[i - 1]) {
-      r = true;
-      s = i - 1;
-    }
-    if (r && !rEnded && line[i] > line[i - 1]) {
-      rEnded = true;
-			good = true;
-      e = i - 1;
-    }
-		if (rEnded) {
-			cout << s << endl;
-			if (s > 0) {
-				if (line[e] < line[s-1]) {
-					good = false;
-				}
-			}
-		}
-    if (i == n && !r) {
-      preSorted = true;
-      good = true;
+    if (l != -1) {
+      if (r == -1 && line[i] < line[i + 1]) {
+        r = i;
+      }
+      if (r == -1 && i == n - 2) {
+        r = i + 1;
+      }
     }
   }
-  if (good) {
-    cout << "yes" << endl;
-    if (preSorted) {
-      s = e = 1;
-    } else if (s == 0) {
-      s = 1;
+  // for (int i : line) {
+  //   cout << i << " ";
+  // }
+  // cout << endl;
+  // cout << l + 1 << " " << r + 1 << endl;
+  reverse(line.begin() + l, line.begin() + r + 1);
+  // for (int i : line) {
+  //   cout << i << " ";
+  // }
+  // cout << endl;
+  // cout << l + 1 << " " << r + 1 << endl;
+  for (int i = 0; i < n - 1; i++) {
+    if (line[i] >= line[i + 1]) {
+      cout << "no" << endl;
+      return 0;
     }
-    if (e < s) {
-      e = n;
-    }
-    cout << s << " " << e << endl;
-  } else {
-    cout << "no" << endl;
   }
+  cout << "yes" << endl;
+  if (l == -1) {
+    l = 0;
+  }
+  if (r == -1) {
+    r = 0;
+  }
+  cout << l + 1 << " " << r + 1;
 }
