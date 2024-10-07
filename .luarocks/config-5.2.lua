bc9c92ccf8814f759809d78796dea0e8eb60707f
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-10-07 23:11:54 +0200

    luarocks

---
+++ .luarocks/config-5.2.lua
@@ -0,0 +1,6 @@
+variables = {
+   LUA = "/nix/store/w8y74kbrcsjh8izmy5vff1chspvdsl0s-lua-5.2.4/bin/lua",
+   LUA_BINDIR = "/nix/store/w8y74kbrcsjh8izmy5vff1chspvdsl0s-lua-5.2.4/bin",
+   LUA_DIR = "/nix/store/w8y74kbrcsjh8izmy5vff1chspvdsl0s-lua-5.2.4",
+   LUA_INCDIR = "/nix/store/w8y74kbrcsjh8izmy5vff1chspvdsl0s-lua-5.2.4/include"
+}
