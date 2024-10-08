f7b12c81e137c439e40f176dc9edfa6bf2967622
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-10-08 20:39:46 +0200

    Yet another Nim Game

---
+++ Gym/TheForce/C1-Yet_Another_Nim_Game/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++) {
+    int n;
+    cin >> n;
+    int p = 1;
+    for (int i = 1; i <= n; i++) {
+			cout << i << " ";
+    }
+    cout << endl;
+  }
+}
